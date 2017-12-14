1249bd35e61a11ac7b0c9e6728a81076a9890ee3
kavall <kavall1998@gmail.com>
2017-12-14 22:42:56 +0300

    GUI

--- Test/Logic/MatrixCountTest.java
+++ Test/Logic/MatrixCountTest.java
@@ -1,14 +1,49 @@
 package Logic;
-
 import static org.junit.Assert.*;
 
 public class MatrixCountTest {
     @org.junit.Test
-    public void countWay() throws Exception {
-        int matrix[][] = {{2,1,1},{1,1,1},{1,1,1}};
+    public void countWayTest1() throws Exception {           //Проверка чтения из файла
+        MatrixReader mr = new MatrixReader();                
+        MatrixCount m = new MatrixCount(mr.mReader());
+        m.launch();
+        assertEquals(m.count,1);
+    }
+    @org.junit.Test
+    public void countWayTest2() throws Exception {           //Проверка с отрицательными числами
+        int [][] matrix = {
+                {1,1,-1},
+                {1,1,-10},
+                {1,1,0}
+        };
         MatrixCount m = new MatrixCount(matrix);
         m.launch();
-        assertEquals(m.count,2);
+        assertEquals(m.count,3);
+    }
+    @org.junit.Test
+    public void countWayTest3() throws Exception {           //Проверка c начальным отрицательным шагом
+        int [][] matrix = {
+                {-1,1,1},
+                {1,1,1},
+                {1,1,0}
+        };
+        MatrixCount m = new MatrixCount(matrix);
+        m.launch();
+        assertEquals(m.count,0);
+    }
+    @org.junit.Test
+    public void countWayTest4() throws Exception {           //Проверка с не еденичными шагами
+        int [][] matrix = {
+                {1,2,1,5},
+                {3,1,1,2},
+                {1,1,1,1},
+                {1,4,5,2},
+                {1,6,3,2},
+                {3,2,3,0}
+        };
+        MatrixCount m = new MatrixCount(matrix);
+        m.launch();
+        assertEquals(m.count,4);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-
+import Logic.MatrixReader;
 import Visualizer.Window;
+
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = {
-                {1, 1, 1, 1},
-                {1, 1, 1, 1},
-                {1, 1, 1, 1},
-                {1, 1, 1, 1}};
-
-        new Window(matrix);
+        MatrixReader m = new MatrixReader();
+        new Window(m.mReader());
     }
+
 }
